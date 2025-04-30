8784b58d526295820682b96db1308b4f993d1962
Karthik <karthiksayi123@gmail.com>
2025-04-30 20:07:25 +0530

    conditionals

---
+++ tests/if_conditional_test_12thclass.spec.js
@@ -0,0 +1,27 @@
+
+const { browser, test, expect } = require('@playwright/test');
+
+test("Click one of the elements that is visible out of two", async ({ page }) => {
+
+    await page.goto("https://the-internet.herokuapp.com/disappearing_elements");
+
+    const contactus = page.getByRole("link", { name: "Contact Us" });
+    const portfolio = page.getByRole("link", { name: "Portfolio" });
+    const gallery = page.getByRole("link", { name: "Gallery" });
+    
+
+    if (await gallery.isVisible()) {
+        await gallery.click();
+        console.log("clicked on Gallery")
+    }
+     else if (await portfolio.isVisible()) {
+        await portfolio.click();
+        console.log("clicked on Portfolio")
+    }
+   
+    for(let i=0 ; i<10; i++){
+
+        await expect(page).toHaveURL(/.*gallery|.*portfolio/);
+    }
+   
+});
